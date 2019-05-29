82b3e4ef828b47daa79479e3884c126d8cb8e0d0
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-05-29 16:17:59 +0300

    onError

---
+++ examples/part5/02_onError/com/agiledeveloper/Sample.java
@@ -0,0 +1,44 @@
+package com.agiledeveloper;
+
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+
+import java.util.*;
+
+public class Sample {
+  public static void main(String[] args) {
+    //failure happens.
+    //Rather than prevent failure, we will gracefully recover
+    //failure is a first class citizen
+    //error is just another form of data
+
+    //we can receive error through the error channel.
+
+    Flowable.<Integer>create(emitter -> emit(emitter), BackpressureStrategy.BUFFER)
+      .map(data -> data * 2)
+      .subscribe(System.out::println,
+          err -> System.out.println("ERROR: " + err));
+  }
+
+  //the blowup was captured and given to use using the error channel.
+
+  private static void emit(FlowableEmitter<Integer> emitter) {
+    int count = 0;
+
+    while(++count < 10) {
+      System.out.println("emitting... " + count);
+      emitter.onNext(count);
+
+      if(count == 7) {
+        //throw new RuntimeException("something went wrong");
+        emitter.onError(new RuntimeException("something went wrong here..."));
+      }
+    }
+
+    //once error is generated, the data channel will close.
+    //even if there is an emit, it becomes a no-op after an error.
+
+    //This idea is based on circuit breaker.
+  }
+}
