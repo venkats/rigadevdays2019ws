fedd8e50a7b1b9d649a69980d58693f44f9caa4a
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-05-29 15:20:23 +0300

    using takeWhile

---
+++ examples/part4/07_using_takeWhile/com/agiledeveloper/Sample.java
@@ -0,0 +1,31 @@
+package com.agiledeveloper;
+
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+
+public class Sample {
+  public static void main(String[] args) {
+    Flowable.<Integer>create(emitter -> emit(emitter), BackpressureStrategy.BUFFER)
+      .takeWhile(data -> data > 70)
+      .subscribe(System.out::println, System.out::println,
+          () -> System.out.println("DONE"));
+
+    //The gate is open at first and once that count is reached,
+    //the gate closes and no more data passes through.
+
+    //take is procedure that will send a dispose (cancel) signal
+    //upstream and send a complete signal downstream.
+  }
+
+  private static void emit(FlowableEmitter<Integer> emitter) {
+    int count = 0;
+
+    while(++count < 100 && !emitter.isCancelled()) {
+      int data = (int)(Math.random() * 100);
+      System.out.println("emitting... " + data);
+      emitter.onNext(data);
+      try { Thread.sleep(1000); } catch(Exception ex) {}
+    }
+  }
+}
